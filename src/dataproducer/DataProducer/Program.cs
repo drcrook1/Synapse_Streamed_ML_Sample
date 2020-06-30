e26377fb04e7512ac6917cff7cd6f4678dd845be
David Crook <dacrook@microsoft.com>
2020-06-30 16:37:14 -0400

    updated for dw connection stubbing

--- src/dataproducer/DataProducer/Program.cs
+++ src/dataproducer/DataProducer/Program.cs
@@ -1,13 +1,22 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace DataProducer
 {
     class Program
     {
+        private const string DWConnStr = "";
         static void Main(string[] args)
         {
+            //
+            // Create SQL Synapse DW Connection
+            //
+            var sqlDWConnection = new SqlConnection(DWConnStr);
+            sqlDWConnection.Open();
+
             var dataCollectors = new List<DataCollector>();
             for(int i = 0; i < 100; i++)
             {
@@ -20,6 +29,12 @@ namespace DataProducer
                     
                     var collectorJson = JsonConvert.SerializeObject(collector, Formatting.Indented);
                     Console.WriteLine(collectorJson);
+
+                    var cmd = new SqlCommand("insert weather into weather", sqlDWConnection);
+                    cmd.ExecuteNonQuery();
+
+                    var cmdtwo = new SqlCommand("insert weatherview into weatherview", sqlDWConnection);
+                    cmdtwo.ExecuteNonQuery();
                 }
             }
         }
